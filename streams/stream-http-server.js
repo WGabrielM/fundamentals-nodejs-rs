9f7aadb62bce6b12e636772d1050f397fc0b7e4b
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2024-06-14 15:10:50 -0300

    create fetch example with buffers array  and async fuction

--- streams/stream-http-server.js
+++ streams/stream-http-server.js
@@ -14,11 +14,18 @@ class InverseNumberStream extends Transform {
 // req => ReadbleStream
 // res => WritableStream
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
+  const buffers = [];
 
-  return req
-    .pipe(new InverseNumberStream())
-    .pipe(res);
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+
+  const fullStreamContent = Buffer.concat(buffers).toString();
+
+  console.log(fullStreamContent);
+
+  return res.end(fullStreamContent);
 });
 
 server.listen(3334);
